fd126a22a00ea051761439ede5bac02ca2ba8f99
dangth97 <105710820+dangth97@users.noreply.github.com>
2023-09-22 11:42:22 +0700

    upload source code

---
+++ admin/inc/config.php
@@ -0,0 +1,49 @@
+<?php
+// Error Reporting Turn On
+ini_set('error_reporting', E_ALL);
+
+// Setting up the time zone
+date_default_timezone_set('UTC');
+
+// Host Name
+$dbhost = 'localhost';
+
+// Database Name
+$dbname = 'ecommerceweb';
+
+// Database Username
+$dbuser = 'root';
+
+// Database Password
+$dbpass = '';
+
+// Defining base url
+define("BASE_URL", "");
+
+// Getting Admin url
+define("ADMIN_URL", BASE_URL . "admin" . "/");
+
+
+if (!isset($_SESSION['data_recorded'])) {
+try {
+	$pdo = new PDO("mysql:host={$dbhost};dbname={$dbname}", $dbuser, $dbpass);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	if (!isset($_SESSION['visited'])) {
+        // Gán session 'visited'
+        $_SESSION['visited'] = true;
+
+        $ipAddress = $_SERVER['REMOTE_ADDR'];
+
+        $currentTime = date("Y-m-d H:i:s");
+
+        $sql = "INSERT INTO tbl_total_user (ipAddress, time) VALUES (:ip_address, :visit_time)";
+        $statement = $pdo->prepare($sql);
+        $statement->bindParam(':ip_address', $ipAddress);
+        $statement->bindParam(':visit_time', $currentTime);
+        $statement->execute();
+    }
+}
+catch( PDOException $exception ) {
+	echo "Connection error :" . $exception->getMessage();
+}
+};
---
+++ admin/total-user.php
@@ -0,0 +1,43 @@
+<?php require_once('header.php'); ?>
+<section class="content-header">
+	<div class="content-header-left">
+		<h1>Total user</h1>
+	</div>
+</section>
+<section class="content">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="box box-info">        
+        <div class="box-body table-responsive">
+          <table id="example1" class="table table-bordered table-hover table-striped">
+			<thead>
+			    <tr>
+			        <th>#</th>
+			        <th>IP Address</th>
+			        <th>Time</th>
+			    </tr>
+			</thead>
+            <tbody>
+            	<?php
+            	$statement = $pdo->prepare("SELECT * FROM tbl_total_user");
+            	$statement->execute();
+            	$result = $statement->fetchAll(PDO::FETCH_ASSOC);
+            	foreach ($result as $row) {
+            		?>
+					<tr>
+	                    <td><?php echo $row['id']; ?></td>
+	                    <td><?php echo $row['ipAddress']; ?></td>
+	                    <td><?php echo $row['time']; ?></td>
+	                </tr>
+            		<?php
+            	}
+            	?>
+            </tbody>
+          </table>
+        </div>
+      </div>
+  
+
+</section>
+
+<?php require_once('footer.php'); ?>
